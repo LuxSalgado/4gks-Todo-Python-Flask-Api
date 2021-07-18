f0b1d8e6dab811e141498b1913859ac249a57500
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-18 19:13:01 +0000

    Creación del método DELETE

--- src/app.py
+++ src/app.py
@@ -11,13 +11,19 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data
-    decoded_object = json.loads(request_body)
-    todos.append(decoded_object)
-    print("Incoming request with the following body", request_body)
-    todos_jsonifeado = jsonify(todos)
-    return todos_jsonifeado
+    request_body = request.data #recibo la info del postman
+    decoded_object = json.loads(request_body) #Transformo el request en un objeto Python
+    todos.append(decoded_object) #Agrego el objeto a la lista de todos
+    print("Incoming request with the following body", request_body) #Mensaje por consola
+    todos_jsonifeado = jsonify(todos) #Transformo la lista en formato JSON
+    return todos_jsonifeado #Regreso la lista en formato JSON
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position) #Elimino el elemento de la lista en el indice -position-, OJO hay que validad que el indice exista
+    print("This is the position to delete: ",position) #Mensaje por consola
+    todos_jsonifeado = jsonify(todos) #Transformo la lista en formato JSON
+    return todos_jsonifeado #Regreso la lista en formato JSON
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
